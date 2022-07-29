8141b945ab8f9a3408ae6d95cbcb24193a78e851
kevin12-del <duramelkenfack@gmail.com>
2022-07-29 14:37:18 +0100

    fix

--- push.js
+++ push.js
@@ -8,13 +8,11 @@ let vapidKeys = {
 push.setVapidDetails('mailto:ktsobgni.megasoft@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
 let sub = {
-    endpoint:
-       "https://wns2-par02p.notify.windows.com/w/?token=BQYAAAAxhi9bddgEd%2bWARbXop4Hg2pJ8i9dBIeb6rdanFnD%2bEQBUV9lLbUhUiGQGpuZFPoP8JylZhbPhOfs0hRZQHLg5SgGJ19fF0CutWTtSbD0vZspm6%2bPNR4ismHr70PxYTExmKnrYWfhh%2b6p0662j2N9g1uPtVk0wugfSthko5gEsMozHXlx%2bXcRlk31anbc23dRaxZzvKShUOT91o%2fabpNi4Klq4DyLG5e0wU7FR116xcXW5rX1UvwmGvOzBwODhr2sFhqW9hXLstRu%2fhb2ZphdB4u7SRKUyJfzksgTBwk2UE4eXPK3%2fjncZfGNMiLdq1wYrgC%2f92NZLMag94L2BjpSCidT2LsB94N0UqXsCy7PQkA%3d%3d",
-    expirationTime:null,
-    keys: {
-        p256dh:
-           "BG3WK4Iu7uxcw05VW1UPSttfbOhipWLZ5YueQFrkDedEDHZ17z6x5UOkyBm2uGpk5XFvKttcXfLs5BmxqzXlPEE",
-        auth:"8w-6B1O8Wgddb0RK1LjcCw"
-}};
+   endpoint:
+      "https://fcm.googleapis.com/fcm/send/cHR6xwqT4IE:APA91bGQvmw2UPcCFeNbNjxkpMnXvofYJo65eZIB11yJo_s8lZUCzJwGJ1wgbF5mAl0lOUUtf4-xAmELT35otZBJCNCXwyxTwDhhQTjtMWFLKSsNMQeQTWd_4wGXJrwe22DL6ieP_Aoj",
+   expirationTime:null,
+   keys:{
+      p256dh:"BJ19QauRuj8Ed9O4Ug4cVpy87SIrqXhcPpBaW_Zz23cCDRJWIefc0mzfQFtGKO2VDXcViVToclLud4ix6jR7jlg",
+      auth:"ThduneS2KidR8LjKXfa5jA"}};
 
 push.sendNotification(sub, 'test message')
